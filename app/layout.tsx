11f9b265aa12b2dcfebbdcf78ba1b8c33570d34e
Dimi <dimianic123@gmail.com>
2026-08-05 09:52:21 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,14 +21,27 @@ export default function RootLayout({
       <body
         style={{
           margin: 0,
+          padding: "16px", // Adaugă spațiu ca cardul să nu atingă marginea ecranului
           minHeight: "100vh",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          backgroundColor: "#000", // sau culoarea ta de fundal
+          backgroundColor: "#000",
+          boxSizing: "border-box",
+          overflowX: "hidden", // Previne scroll-ul orizontal nedorit
         }}
       >
-        {children}
+        {/* Un wrapper responsive care strânge cardul */}
+        <div
+          style={{
+            width: "100%",
+            maxWidth: "400px", // Ajustează valoarea dacă vrei să fie mai lat/îngust pe desktop
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          {children}
+        </div>
       </body>
     </html>
   );
