4fffbb80a2cebec5bfd213c05386fba5963634c1
Dimi <dimianic123@gmail.com>
2026-08-31 13:08:56 +0300

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+
+Robots · TS
+import type { MetadataRoute } from "next";
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin-x7k2", "/gest-x4p7", "/api/"],
+      },
+    ],
+    sitemap: "https://scanvogue.ro/sitemap.xml",
+  };
+}
