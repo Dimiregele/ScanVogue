136ac8912666f5a65eb89c47844a7d71f574cf18
Dimi <dimianic123@gmail.com>
2026-08-31 13:07:19 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
 import ScanVogueLanding from "./scan-vogue-landing";
-
+ 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "SoftwareApplication",
+  name: "ScanVogue",
+  applicationCategory: "BusinessApplication",
+  operatingSystem: "Web",
+  description:
+    "Platformă de gestionare a recenziilor și reclamațiilor pentru restaurante, cafenele și hoteluri din România, cu triaj automat prin inteligență artificială.",
+  offers: {
+    "@type": "Offer",
+    price: "200",
+    priceCurrency: "RON",
+  },
+  provider: {
+    "@type": "Organization",
+    name: "ScanVogue",
+    url: "https://scanvogue.ro",
+    email: "scanvogue@gmail.com",
+  },
+  areaServed: {
+    "@type": "Country",
+    name: "România",
+  },
+};
+ 
 export default function Home() {
-  return <ScanVogueLanding />;
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <ScanVogueLanding />
+    </>
+  );
 }
